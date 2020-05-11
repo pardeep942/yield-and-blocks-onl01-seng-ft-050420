18b070c06eca6b40c146fb22ea5effeb6ca9e93c
PARDEEP KAUR <pkaur84@aol.com>
2020-05-11 05:46:49 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,9 @@
 def hello_t(array)
     i = 0
     while i < array.length
-      yield(array[i])
+      yield array[i]
       i = i + 1
     end
-    array
 end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
